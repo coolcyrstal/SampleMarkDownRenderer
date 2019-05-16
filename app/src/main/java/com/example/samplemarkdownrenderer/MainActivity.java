91c2e99ca96544503cd4cf0f72b924046dec3232
Chayen Tansritrang <chayen.t@extreme.co.th>
2019-05-16 11:36:19 +0700

    add androidx add custom editor

--- app/src/main/java/com/example/samplemarkdownrenderer/MainActivity.java
+++ app/src/main/java/com/example/samplemarkdownrenderer/MainActivity.java
@@ -5,7 +5,6 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.main_text_view)
     TextView mTextView;
-    @BindView(R.id.main_edit_text)
-    EditText mEditText;
+//    @BindView(R.id.main_edit_text)
+//    EditText mEditText;
     @BindView(R.id.toggle_button_layout)
     ToggleButtonLayout mToggleButtonLayout;
 
@@ -52,28 +51,28 @@ public class MainActivity extends AppCompatActivity {
         final Markwon markwon = Markwon.create(getApplicationContext());
         markwon.setMarkdown(mTextView, mTextTemp);
 
-        mEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(start+before < mTextLength){
-                    mTextLength -= 1;
-                } else {
-                    mTextLength += 1;
-                    mTextTemp += String.valueOf(s.charAt(start + before));
-                }
-                markwon.setMarkdown(mTextView, mFullText + mMarkDownPrefixExpression + mTextTemp + mMarkDownSuffixExpression);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
+//        mEditText.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                if(start+before < mTextLength){
+//                    mTextLength -= 1;
+//                } else {
+//                    mTextLength += 1;
+//                    mTextTemp += String.valueOf(s.charAt(start + before));
+//                }
+//                markwon.setMarkdown(mTextView, mFullText + mMarkDownPrefixExpression + mTextTemp + mMarkDownSuffixExpression);
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//
+//            }
+//        });
 
         mToggleButtonLayout.setOnToggledListener(new Function2<Toggle, Boolean, Unit>() {
             @Override
